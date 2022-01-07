5f2355637cc652ac70399646556c1cad0bfb40ef
carlosgonzalez96 <carlosglez817@gmail.com>
2022-01-07 14:06:04 -0800

    setup redux

--- src/components/signUp/signUpComponent.js
+++ src/components/signUp/signUpComponent.js
@@ -26,7 +26,6 @@ class SignUp extends React.Component {
         const { displayName, email, password, confirmPassword } = this.state;
 
         if(password !== confirmPassword) {
-            // alert("passwords don't match");
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
