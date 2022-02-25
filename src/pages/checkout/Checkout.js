eb62f901039a2099c5c3faf6e467edd46fc1e61c
carlosgonzalez96 <carlosglez817@gmail.com>
2022-02-25 10:24:28 -0800

    Added items to cart will now display on checkout page

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout item/CheckoutItem';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkoutStyles.scss';
@@ -27,7 +29,7 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem =>
-                cartItem.name
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
             )
         }
         <div className='total'><span>${total}</span></div>
