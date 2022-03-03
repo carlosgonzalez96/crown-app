4d0070da8773191539fd2c06fa0b45c4eddfbf25
carlosgonzalez96 <carlosglez817@gmail.com>
2022-03-03 11:53:37 -0800

    created respective ux flow directories and moved directory and shop data into its own reducer

--- src/pages/shop/shopComponent.js
+++ src/pages/shop/shopComponent.js
@@ -1,27 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shopData';
-import CollectionPreview from '../../components/preview-collection/collection-preview';
+import CollectionPage from '../collection/Collection';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ match }) =>  (
 
-        this.state = {
-            collections: SHOP_DATA, 
-        }
-    }
-
-    render() {
-
-        const {collections} = this.state;
-        return(<div className='shop-page'>
-                {collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id}{...otherCollectionProps}/>
-                ))}
-            </div>
-        );
-    }
-}
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
 export default ShopPage;
